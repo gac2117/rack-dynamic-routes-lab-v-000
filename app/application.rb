f89d05e19e80affa1f7bb1e7f3d2144591c9e3c5
Grace Choi <oiograceoio@gmail.com>
2019-03-26 17:12:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,14 +8,19 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
-      resp.status = 200
+      @@items.find do |i|
+        if i.name == item_name
+          resp.write i.price 
+          resp.status = 200
+        else
+          resp.write "Item not found"
+          resp.status = 404
+        end
+      end
     else
       resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
-
   end
 end
