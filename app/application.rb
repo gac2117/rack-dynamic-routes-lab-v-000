8a7f2331ad4e73c533893d290dffb4780932a128
Grace Choi <oiograceoio@gmail.com>
2019-03-26 17:05:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/item/)
-      item_name = req.path.split("/item/").last
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
       resp.statue = 200
