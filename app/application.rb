08e9267646697251f825fbcec3ad137e90dba911
Grace Choi <oiograceoio@gmail.com>
2019-03-26 16:59:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,10 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/item/)
-
+      item_name = req.path.split("/item/").last
+      resp.write item_name.price 
     else
-      resp.write "Path not foun"
+      resp.write "Path not found"
       resp.status = 404
     end
   end
